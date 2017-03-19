0f5dbe6d750053b878883070f54a4bd86d0a3d7f
dsgrafiniert <ds@grafiniert.de>
2017-03-19 11:06:58 +0100

    Update duplicity_script.sh

--- duplicity_script.sh
+++ duplicity_script.sh
@@ -20,7 +20,7 @@ gpg --list-keys > /dev/null
 gpg --import /certs/gpg-key.asc > /dev/null
 gpg --allow-secret-key-import --import /certs/gpg-secret-key.asc > /dev/null
 gpg --import-ownertrust /certs/ownertrust.gpg.txt
-duplicity --tempdir=/tmp --volsize=250 --gpg-options "--compress-level=$COMPRESS_LEVEL" --asynchronous-upload --archive-dir=/duplicity_archive -v7 --full-if-older-than $FULL_IF_OLDER_THAN --encrypt-key=$ENC_KEY --sign-key=$SIGN_KEY --include-globbing-filelist /config/incexcllist /volume1 $TARGET
+duplicity --tempdir=/tmp --volsize=250 --gpg-options "--compress-level=$COMPRESS_LEVEL" --asynchronous-upload --archive-dir=/duplicity_archive -v7 --full-if-older-than $FULL_IF_OLDER_THAN --encrypt-key=$ENC_KEY --sign-key=$SIGN_KEY --include-globbing-filelist /config/incexcllist /mnt $TARGET
 
 duplicity remove-all-but-n-full $KEEP_FULL -v3 --tempdir=/tmp --archive-dir=/duplicity_archive  --full-if-older-than 14D --force --encrypt-key=$ENC_KEY --sign-key=$SIGN_KEY $TARGET
 
