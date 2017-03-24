f913696df8d1de3146c3a45008713ce0e997e22f
dsgrafiniert <ds@grafiniert.de>
2017-03-24 19:07:43 +0100

    Update duplicity_script.sh

--- duplicity_script.sh
+++ duplicity_script.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 LOCKFILE=/tmp/AXgqg0lsoeykp9L9NZjIuaqvu7ANILL4foeqzpJcTs3YkwtiJ0
 
 if [ -e ${LOCKFILE} ] && kill -0 `cat ${LOCKFILE}`; then
@@ -17,7 +18,9 @@ gpg --list-keys > /dev/null
 gpg --import /certs/gpg-key.asc > /dev/null
 gpg --allow-secret-key-import --import /certs/gpg-secret-key.asc > /dev/null
 gpg --import-ownertrust /certs/ownertrust.gpg.txt
-duplicity --tempdir=/tmp --volsize=250 --gpg-options "--compress-level=$COMPRESS_LEVEL" --asynchronous-upload --archive-dir=/duplicity_archive -v7 --full-if-older-than $FULL_IF_OLDER_THAN --encrypt-key=$ENC_KEY --sign-key=$SIGN_KEY --include-globbing-filelist /config/incexcllist /mnt $TARGET
+env PASSPHRASE=28acaneltO
+env SIGN_PASSPHRASE=28acaneltO
+duplicity --tempdir=/tmp --volsize=40 --gpg-options "--pinentry-mode loopback --compress-level=$COMPRESS_LEVEL" --asynchronous-upload --archive-dir=/duplicity_archive -v3 --full-if-older-than $FULL_IF_OLDER_THAN --encrypt-key=$ENC_KEY --sign-key=$SIGN_KEY --include-globbing-filelist /config/incexcllist /mnt $TARGET
 
 duplicity remove-all-but-n-full $KEEP_FULL -v3 --tempdir=/tmp --archive-dir=/duplicity_archive  --full-if-older-than 14D --force --encrypt-key=$ENC_KEY --sign-key=$SIGN_KEY $TARGET
 
