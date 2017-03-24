89866eb8133f0cbf7f0e403b41635ac7e9b7a8da
dsgrafiniert <ds@grafiniert.de>
2017-03-25 00:11:19 +0100

    Update duplicity_script.sh

--- duplicity_script.sh
+++ duplicity_script.sh
@@ -18,8 +18,8 @@ gpg --list-keys > /dev/null
 gpg --import /certs/gpg-key.asc > /dev/null
 gpg --allow-secret-key-import --import /certs/gpg-secret-key.asc > /dev/null
 gpg --import-ownertrust /certs/ownertrust.gpg.txt
-env PASSPHRASE=28acaneltO
-env SIGN_PASSPHRASE=28acaneltO
+env PASSPHRASE=$PASS
+env SIGN_PASSPHRASE=$SIGN_PASS
 duplicity --tempdir=/tmp --volsize=40 --gpg-options "--pinentry-mode loopback --compress-level=$COMPRESS_LEVEL" --asynchronous-upload --archive-dir=/duplicity_archive -v3 --full-if-older-than $FULL_IF_OLDER_THAN --encrypt-key=$ENC_KEY --sign-key=$SIGN_KEY --include-globbing-filelist /config/incexcllist /mnt $TARGET
 
 duplicity remove-all-but-n-full $KEEP_FULL -v3 --tempdir=/tmp --archive-dir=/duplicity_archive  --full-if-older-than 14D --force --encrypt-key=$ENC_KEY --sign-key=$SIGN_KEY $TARGET
