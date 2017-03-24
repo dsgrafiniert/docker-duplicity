ca707a4f69f33fcca2184d3c083555ff75a5f342
dsgrafiniert <ds@grafiniert.de>
2017-03-25 00:18:48 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 envsubst '$PASS $SIGN_PASS $COMPRESS_LEVEL $FULL_IF_OLDER_THAN $ENC_KEY $SIGN_KEY $TARGET $KEEP_FULL' < /opt/duplicity/duplicity_script.sh.template > /opt/duplicity/duplicity_script.sh
 chmod +x /opt/duplicity/duplicity_script.sh
+cron && tail -f /var/log/cron.log
