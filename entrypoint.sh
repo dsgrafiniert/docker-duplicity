0430e538becb88f0720f9b5284ae5c3030558758
Dominik Schön <ds@grafiniert.de>
2017-09-01 09:12:21 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 envsubst '$PASS $SIGN_PASS $COMPRESS_LEVEL $FULL_IF_OLDER_THAN $ENC_KEY $SIGN_KEY $TARGET $KEEP_FULL' < /opt/duplicity/duplicity_script.sh.template > /opt/duplicity/duplicity_script.sh
 chmod +x /opt/duplicity/duplicity_script.sh
-/bin/bash /opt/duplicity/duplicity_script.sh
+/bin/bash /opt/duplicity/duplicity_script.sh > /var/log/test.log
